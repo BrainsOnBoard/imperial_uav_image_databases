9ba930f4462962d46642234f86512decb1473b74
Alex Dewar <a.dewar@sussex.ac.uk>
2021-02-09 10:51:31 +0000

    Parallelise extraction for multiple files + save metadata

--- bag2image_database.py
+++ bag2image_database.py
@@ -12,69 +12,91 @@ www.speal.ca
 
 import rosbag
 import sys
-import csv
-import time
-import string
 import os  # for file management make directory
-import shutil  # for file management, copy file
 from cv_bridge import CvBridge
 import cv2
-import rospy
 import numpy as np
 import math
+import multiprocessing
+import joblib
 
+assert len(sys.argv) > 1
 
-assert len(sys.argv) == 2
+def process_file(filepath):
+    bridge = CvBridge()
 
-bridge = CvBridge()
+    bag = rosbag.Bag(filepath)
+    bag_name = bag.filename
 
-bag = rosbag.Bag(sys.argv[1])
-bagName = bag.filename
+    print('Processing ' + bag_name)
 
-print('Processing ' + bagName)
+    # create a new directory
+    folder = bag_name.rsplit('.', 1)[0]
 
-# create a new directory
-folder = bagName.rsplit('.', 1)[0]
+    try:  # else already exists
+        os.makedirs(folder)
+    except:
+        pass
 
-try:  # else already exists
-    os.makedirs(folder)
-except:
-    pass
+    # Save camera parameters for unwrapping code
+    with open(os.path.join(folder, 'database_metadata.yaml'), 'w') as yaml:
+        yaml.write('''%YAML:1.0
+---
+metadata:
+  type: route
+  camera:
+     name: pixpro_usb
+     resolution: [ 1440, 1440 ]
+     isPanoramic: 1
+  needsUnwrapping: 1
+  isGreyscale: 0
+''')
 
-times = []
-poses = np.empty((0, 6))
-for _, msg, t in bag.read_messages('/vicon/dd/odom'):
-    times.append(t.to_sec())
-    pose = msg.pose.pose
-    pose_arr = [pose.position.x, pose.position.y, pose.position.z,
-                pose.orientation.x, pose.orientation.y, pose.orientation.z]
-    poses = np.concatenate((poses, [pose_arr]))
+    # Extract drone's poses with timestamps from bag for later interpolation
+    times = []
+    poses = np.empty((0, 6))
+    for _, msg, t in bag.read_messages(['/vicon/dd/odom', '/vicon/f450/odom']):
+        times.append(t.to_sec())
+        pose = msg.pose.pose
+        pose_arr = [pose.position.x, pose.position.y, pose.position.z,
+                    pose.orientation.x, pose.orientation.y, pose.orientation.z]
+        poses = np.concatenate((poses, [pose_arr]))
 
-topics = set()
-start_time = bag.get_start_time()
-with open(os.path.join(folder, 'database_entries.csv'), 'w') as csv:
-    csv.write('Timestamp [ms], X [mm], Y [mm], Z [mm], Heading [degrees], ' +
-              'Pitch [degrees], Roll [degrees], Filename\n')
+    # Create CSV file + extract JPEGs
+    start_time = bag.get_start_time()
+    with open(os.path.join(folder, 'database_entries.csv'), 'w') as csv:
+        csv.write('Timestamp [ms], X [mm], Y [mm], Z [mm], Heading [degrees], ' +
+                'Pitch [degrees], Roll [degrees], Filename\n')
 
-    count = 0
-    for _, msg, t in bag.read_messages('/kodak/image_raw'):
-        time = t.to_sec()
-        try:
-            gt_idx = next(i for i, val in enumerate(times) if val > time)
-        except StopIteration:
-            break
+        count = 0
+        for _, msg, t in bag.read_messages('/kodak/image_raw'):
+            time = t.to_sec()
 
-        csv.write('%f, ' % (1000 * (time - start_time)))
+            # Find the first pose reading after this frame or quit
+            try:
+                gt_idx = next(i for i, val in enumerate(times) if val > time)
+            except StopIteration:
+                break
 
-        lt_idx = gt_idx - 1
-        rng = times[gt_idx] - times[lt_idx]
-        prop = (time - times[lt_idx]) / rng
-        p = poses[lt_idx, :] + prop * (poses[gt_idx, :] - poses[lt_idx, :])
-        csv.write('%f, %f, %f, ' % (p[0] * 1000, p[1] * 1000, p[2] * 1000))
-        csv.write('%f, %f, %f, ' % (math.degrees(p[3]), math.degrees(p[4]), math.degrees(p[5])))
+            csv.write('%f, ' % (1000 * (time - start_time)))
 
-        filename = 'image%05d.jpg' % count
-        csv.write(filename + '\n')
-        count += 1
-        im = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        assert cv2.imwrite(os.path.join(folder, filename), im)
+            # Linearly interpolate position to guess where drone was when image
+            # was recorded
+            lt_idx = gt_idx - 1
+            rng = times[gt_idx] - times[lt_idx]
+            prop = (time - times[lt_idx]) / rng
+            p = poses[lt_idx, :] + prop * (poses[gt_idx, :] - poses[lt_idx, :])
+            csv.write('%f, %f, %f, ' % (p[0] * 1000, p[1] * 1000, p[2] * 1000))
+            csv.write('%f, %f, %f, ' %
+                    (math.degrees(p[3]), math.degrees(p[4]), math.degrees(p[5])))
+
+            filename = 'image%05d.jpg' % count
+            csv.write(filename + '\n')
+            count += 1
+            im = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+            assert cv2.imwrite(os.path.join(folder, filename), im)
+
+    print('Finished ' + bag_name)
+
+num_cores = multiprocessing.cpu_count()
+joblib.Parallel(n_jobs=num_cores)(joblib.delayed(process_file)(path) for path in sys.argv[1:])
